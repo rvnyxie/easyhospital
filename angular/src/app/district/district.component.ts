d327d14f7c2d54b80e90ddaa334ea10877bd1247
unknown <thanhnam.awa@gmail.com>
2024-12-26 08:46:52 +0700

    ng: feat: add simple district crud

--- angular/src/app/district/district.component.ts
+++ angular/src/app/district/district.component.ts
@@ -1,10 +1,113 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ListService, PagedResultDto } from '@abp/ng.core';
+import { LocalityPagedAndSortedResultRequestDto } from '@proxy/base';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DistrictDto, DistrictService } from '@proxy/districts';
 
 @Component({
   selector: 'app-district',
   templateUrl: './district.component.html',
-  styleUrl: './district.component.scss'
+  styleUrl: './district.component.scss',
+  providers: [ListService],
 })
-export class DistrictComponent {
+export class DistrictComponent implements OnInit {
+  district = { totalCount: 0, items: [] } as PagedResultDto<DistrictDto>;
+  query = {
+    skipCount: 0,
+    maxResultCount: 10,
+    sorting: '',
+    search: '',
+  } as LocalityPagedAndSortedResultRequestDto;
 
+  // Modal
+  isModalOpen = false;
+  modalMode: 'create' | 'update' | 'delete' = 'create'; // Current mode
+  modalTitle = 'Default modal title';
+  deleteMessage = '';
+  districtForm: FormGroup; // Reactive form for create/update
+  selectedDistrict: DistrictDto;
+
+  constructor(public list: ListService,
+              private districtService: DistrictService,
+              private fb: FormBuilder) {
+    this.districtForm = this.fb.group({
+      id: [''],
+      code: ['', [Validators.required]],
+      name: ['', [Validators.required]],
+      englishName: [''],
+      level: ['', [Validators.required]]
+    });
+  }
+
+  ngOnInit() {
+    const districtStreamCreator = (query: LocalityPagedAndSortedResultRequestDto) => this.districtService.getList(query);
+
+    this.list.hookToQuery(districtStreamCreator).subscribe((response) => {
+      this.district = response;
+      console.log(this.district);
+    });
+  }
+
+  onQueryParamsChange(params: { pageIndex: number; pageSize: number; sort: { key: string; value: string }[] }) {
+    this.list.get();
+  }
+
+  handleModalCancel() {
+    console.log("Cancel");
+    this.closeModal();
+  }
+
+  handleModalConfirm(): void {
+    if (this.modalMode === 'create' || this.modalMode === 'update') {
+      if (this.districtForm.valid) {
+        const formData = this.districtForm.value;
+        console.log(formData);
+        if (this.modalMode === 'create') {
+          this.districtService.create(formData).subscribe((response) => {
+            this.isModalOpen = false;
+            console.log(response);
+            this.list.get();
+          });
+        } else if (this.modalMode === 'update') {
+          this.districtService.update(formData.id, formData).subscribe(() => {
+            this.isModalOpen = false;
+            this.list.get();
+          });
+        }
+      }
+    } else if (this.modalMode === 'delete') {
+      this.districtService.delete(this.selectedDistrict.id).subscribe(() => {
+        this.isModalOpen = false;
+        this.list.get();
+      });
+    }
+  }
+
+  openModal(mode: 'create' | 'update' | 'delete', district?: DistrictDto): void {
+    this.isModalOpen = true;
+    this.modalMode = mode;
+
+    if (mode === 'create') {
+      this.modalTitle = 'Create District';
+      this.districtForm.reset();
+      this.selectedDistrict = null;
+    } else if (mode === 'update') {
+      this.modalTitle = 'Update District';
+      this.districtForm.patchValue(district); // Populate form with existing data
+      this.selectedDistrict = district;
+    } else if (mode === 'delete') {
+      this.modalTitle = 'Delete District';
+      this.deleteMessage = `Are you sure you want to delete the district "${district?.name}"?`;
+      this.selectedDistrict = district;
+    }
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.selectedDistrict = null;
+  }
+
+  get isFormMode(): boolean {
+    return this.modalMode === 'create' || this.modalMode === 'update';
+  }
 }
--- angular/src/app/district/district.module.ts
+++ angular/src/app/district/district.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 import { DistrictRoutingModule } from './district-routing.module';
 import { DistrictComponent } from './district.component';
 import { SharedModule } from '../shared/shared.module';
+import { NzTableComponent, NzTableModule, NzThAddOnComponent, NzThMeasureDirective } from 'ng-zorro-antd/table';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzModalContentDirective, NzModalModule } from 'ng-zorro-antd/modal';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputDirective } from 'ng-zorro-antd/input';
 
 
 @NgModule({
@@ -11,7 +16,16 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [
     SharedModule,
-    DistrictRoutingModule
+    DistrictRoutingModule,
+    NzTableComponent,
+    NzTableModule,
+    NzThMeasureDirective,
+    NzThAddOnComponent,
+    NzButtonModule,
+    NzModalModule,
+    NzModalContentDirective,
+    NzFormModule,
+    NzInputDirective
   ]
 })
 export class DistrictModule { }
